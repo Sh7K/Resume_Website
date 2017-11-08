40d2aab4042b488180bd79d2b59a73bb0cdf3b04
kshameed7@gmail.com <kshameed7@gmail.com>
2017-11-08 23:00:52 +0530

    reverting the mobile menu click change

--- js/index.js
+++ js/index.js
@@ -1,46 +1,28 @@
 $("document").ready(function () {
-  $("img").addClass("animated fadeInDown");
-  $("h1").addClass("animated fadeInDown");
-  $("h2").addClass("animated fadeInDown");
-  $("h3").addClass("animated fadeInDown");
-  $("h4").addClass("animated fadeInDown");
-  $("h5").addClass("animated fadeInDown");
-  $("h6").addClass("animated fadeInDown");
-  $("p").addClass("animated fadeInDown");
-  $("hr").addClass("animated fadeInDown");
-  $("i").addClass("animated fadeInDown");
-  $("section").addClass("animated fadeInDown");
-  $("article").addClass("animated fadeInDown");
-  $("ul").addClass("animated fadeInDown");
+    $("img").addClass("animated fadeInDown");
+    $("h1").addClass("animated fadeInDown");
+    $("h2").addClass("animated fadeInDown");
+    $("h3").addClass("animated fadeInDown");
+    $("h4").addClass("animated fadeInDown");
+    $("h5").addClass("animated fadeInDown");
+    $("h6").addClass("animated fadeInDown");
+    $("p").addClass("animated fadeInDown");
+    $("hr").addClass("animated fadeInDown");
+    $("i").addClass("animated fadeInDown");
+    $("section").addClass("animated fadeInDown");
+    $("article").addClass("animated fadeInDown");
+    $("ul").addClass("animated fadeInDown");
+    
+    $.scrollIt();
+    $('#navicon').click(function() {
+          $('.nav').css({
+              'transform': 'translateX(0)'
+          });
+      });
   
-  $.scrollIt();
-  $('#navicon').click(function() {
-        $('.nav').css({
-            'transform': 'translateX(0)'
-        });
-    });
-
-    $('.nav').click(function() {
-        $('.nav').css({
-            'transform': 'translateX(-200px)'
-        });
-    });
-
-    $('li').click(function() {
-        this.onclick=function(){}
-    });
-});
-
-$('body').click(function() {
-  console.log('clicked');
-        $('.nav').css({
-            'transform': 'translateX(-200px)'
-        });
-        _exit();
-    });
-
-    function _exit() {
-        $('.nav').css({
-            'transform': 'translateX(-200px)'
-        });
-    };
+      $('.nav').click(function() {
+          $('.nav').css({
+              'transform': 'translateX(-200px)'
+          });
+      });
+  });
